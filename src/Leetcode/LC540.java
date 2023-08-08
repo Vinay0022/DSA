17605c086c30db16c85087c62847d7c83ec48b71
Vinay0022 <vinu031995@gmail.com>
2023-08-08 17:59:25 +0530

    LC540-s

---
+++ src/Leetcode/LC540.java
@@ -0,0 +1,31 @@
+package com.mycompany.app;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC540{
+  public static void main(String[] args) {
+    //2,1,2,2,0,0,2,0
+    //3,3,7,7,10,11,11
+    int []nums = {3,3,7,7,10,11,11};
+    System.out.println(sol(nums));
+}
+  public static int sol(int[]nums){
+int [] arr = new int[nums.length*2];
+for (int i = 0; i < nums.length; i++) {
+      if (nums[i]!=0) {
+ int correctI = nums[i] -1; 
+      arr[correctI]++  ;
+      }
+}
+    System.out.println(Arrays.toString(arr));
+    for (int i = 0; i < arr.length; i++) {
+     if (arr[i]==1) {
+     return  i+1;
+     } 
+    }
+    return 0;
+  }
+
+}
